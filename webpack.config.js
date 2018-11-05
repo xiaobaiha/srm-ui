648b6b569d45391f0fdf7f532ffc6a3f902ee85a
feizhijun@momenta <feizhijun@momenta.ai>
2018-11-05 20:20:36 +0800

    feat: dev build

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,8 @@ module.exports = {
         } 
       },
       { 
-        test: /\.css$/, 
-        use: ['style-loader','css-loader']
+        test: /\.(css|scss)$/, 
+        use: ['style-loader','css-loader', 'sass-loader']
       }
     ]
   },
